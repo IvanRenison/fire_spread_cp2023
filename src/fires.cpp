6e71af57dcd4503ae281d336c6f25a7c32b26bd5
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-03-20 07:47:10 -0300

    Add `FireStats` struct

---
+++ src/fires.cpp
@@ -0,0 +1,32 @@
+#include "fires.hpp"
+
+#include "landscape.hpp"
+#include "matrix.hpp"
+
+FireStats get_fire_stats(Fire fire, Landscape landscape) {
+
+  FireStats stats = {0, 0, 0, 0};
+
+  for (std::pair<uint, uint> cell_xy : fire.burned_ids) {
+    uint x = cell_xy.first;
+    uint y = cell_xy.second;
+
+    Cell cell = landscape[x, y];
+
+    if (cell.subalpine) {
+      stats.counts_veg_subalpine++;
+    }
+    else if (cell.wet) {
+      stats.counts_veg_wet++;
+    }
+    else if (cell.dry) {
+      stats.counts_veg_dry++;
+    }
+    else {
+      stats.counts_veg_matorral++;
+    }
+  }
+
+  return stats;
+
+}
--- src/fires.hpp
+++ src/fires.hpp
@@ -6,14 +6,25 @@
 
 #include "types.h"
 
+#include "landscape.hpp"
+#include "matrix.hpp"
+
 typedef struct _s_Fire {
   uint width;
   uint height;
 
-  bool* burned_layer;
+  Matrix<bool> burned_layer;
 
   std::vector<std::pair<uint, uint>> burned_ids;
 } Fire;
 
+typedef struct _s_FireStats {
+  uint counts_veg_matorral;
+  uint counts_veg_subalpine;
+  uint counts_veg_wet;
+  uint counts_veg_dry;
+} FireStats;
+
+FireStats get_fire_stats(Fire fire, Landscape landscape);
 
 #endif
