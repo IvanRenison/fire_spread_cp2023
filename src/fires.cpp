6b67546d5688331970007331ca842cae8a2e9089
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-03-26 20:10:25 -0300

    Validate fires data in `read_fire`

--- src/fires.cpp
+++ src/fires.cpp
@@ -6,6 +6,11 @@
 Fire read_fire(uint width, uint height, std::string filename_prefix) {
 
   std::ifstream burned_ids_file(filename_prefix + "-burned_ids.csv");
+
+  if (!burned_ids_file.is_open()) {
+    throw std::runtime_error("Can't open landscape file");
+  }
+
   CSVIterator loop(burned_ids_file);
   loop++;
 
@@ -15,8 +20,14 @@ Fire read_fire(uint width, uint height, std::string filename_prefix) {
   burned_ids.reserve(width * height);
 
   for (; loop != CSVIterator(); ++loop) {
+    if (loop->size() < 2) {
+      throw std::runtime_error("Invalid fire file");
+    }
     uint x = atoi((*loop)[0].data());
     uint y = atoi((*loop)[1].data());
+    if (x >= width || y >= height) {
+      throw std::runtime_error("Invalid fire file");
+    }
     burned_layer[x, y] = true;
     burned_ids.push_back({ x, y });
   }
