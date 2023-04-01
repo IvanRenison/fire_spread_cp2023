c0dccb5f759aeb24b9903a3339a6abf1bfe8f260
Santiago Monserrat Campanello <santimonserr@gmail.co>
2023-04-01 15:55:12 -0300

    allow csv path prefix as command line arguments

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -1,9 +1,13 @@
 import subprocess
+import sys
+
+if len(sys.argv) == 1:
+    raise ValueError('Not landscapes where passed as arguments')
 
 compilers = ['g++', 'clang++-16']
 CPPOPTFLAGS = ['0', '1', '2', '3', 'fast']
 march_options = ['', '-march=native']
-landscapes = ['2021_865', '1999_25j', '2013_12']
+landscapes_paths_prefix = sys.argv[1:0] 
 n_particles = 30
 m_same_particles = 2
 
@@ -18,7 +22,8 @@ for compiler in compilers:
     for flag in CPPOPTFLAGS:
         for march_option in march_options:
             string += compile_sh(compiler, flag, march_option)
-            for landscape in landscapes:
-                string += f' && ./a.out ../csv/{landscape} {n_particles} {m_same_particles} >> {compiler}_{flag}{march_option}_{landscape}.csv'
+            for landscape_path in landscapes_paths_prefix:
+                landscape_name = landscape_path.split('/')[-1]
+                string += f' && ./a.out {landscape_path} {n_particles} {m_same_particles} >> {compiler}_{flag}{march_option}_{landscape_path}.csv'
 
 subprocess.run(string, shell=True)
