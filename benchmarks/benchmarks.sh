b3bfa13bd54d700990fa8beae828c0d4f2f62a52
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-04-03 14:01:57 -0300

    Update `benchmarks.sh`

--- benchmarks/benchmarks.sh
+++ benchmarks/benchmarks.sh
@@ -1,10 +1,10 @@
 
 # Read names of all cpp files except main.cpp and delete extension
 
-files_cpp=$(ls ../*.cpp | grep -v main.cpp)
+files_cpp=$(ls ../src/*.cpp | grep -v main.cpp)
 
 # Remove ../ and replace .cpp with .o in all
-files_o=$(echo $files_cpp | sed 's/\.\.\///g' | sed 's/\.cpp/\.o/g')
+files_o=$(echo $files_cpp | sed 's/\.\.\/src\///g' | sed 's/\.cpp/\.o/g')
 
 # Define several methods of compiling a main file
 # $1: compiler
@@ -13,12 +13,12 @@ files_o=$(echo $files_cpp | sed 's/\.\.\///g' | sed 's/\.cpp/\.o/g')
 # $4: Optimization level
 
 with_object_files() {
-  $1 -std=c++23 -DNDEBUG -O$4 -I../ -c $files_cpp
-  $1 -std=c++23 -DNDEBUG -O$4 -I../ $files_o $2 -o $3
+  $1 -std=c++23 -DNDEBUG -O$4 -I../src -c $files_cpp
+  $1 -std=c++23 -DNDEBUG -O$4 -I../src $files_o $2 -o $3
 }
 
 without_object_files() {
-  $1 -std=c++23 -O$4 -I../ $files_cpp $2 -o $3
+  $1 -std=c++23 -O$4 -I../src $files_cpp $2 -o $3
 }
 
 benchmarks=$(ls ./*.cpp)
