3dc894325509d9342f840c74449232c593544b09
Santiago Monserrat Campanello <santimonserr@gmail.co>
2023-04-08 13:40:12 -0300

    fix test

--- benchmarks/process_csv.py
+++ benchmarks/process_csv.py
@@ -3,12 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from os.path import commonprefix
 
+
 def plotGraph(name):
-    plt.setp( ax.xaxis.get_majorticklabels(), rotation=45, ha="right" )
+    plt.setp(ax.xaxis.get_majorticklabels(), rotation=30, ha="right")
     plt.gcf().subplots_adjust(bottom=0.15)
-    #plt.legend()
+    # plt.legend()
     plt.title(f'Compillation flags comparasion: {name}')
-    #plt.xlabel('Burned Cells per seconds')
+    # plt.xlabel('Burned Cells per seconds')
     plt.ylabel('Burned Cells per seconds')
     plt.grid(axis='y', alpha=0.75)
     plt.show()
@@ -27,10 +28,12 @@ if (len(sys.argv) == 1):
 else:
     fig, ax = plt.subplots()
     prefix_string = commonprefix(sys.argv[1:])
+    suffix_string = commonprefix([file[::-1] for file in sys.argv[1:]])[::-1]
     for file in sys.argv[1:]:
         df = pd.read_csv(file, names=[
             'burned_cell_per_time', 'amount_of_cells', 'elapsed'], usecols=['burned_cell_per_time'])
         # plt.hist(df['burned_cell_per_time'], alpha=0.5,
         #          label= file.replace('csv', '').replace('.', '').replace('/', '').replace('_', ' '), bins=100, rwidth=0.75)
-        plt.bar(file.replace(prefix_string,'').replace('.csv', ''),df['burned_cell_per_time'].mean(),yerr=df['burned_cell_per_time'].std())
-    plotGraph(''.join(prefix_string.split('/')[-1]))
+        plt.bar(file.removeprefix(prefix_string).removesuffix(suffix_string),
+                df['burned_cell_per_time'].mean(), yerr=df['burned_cell_per_time'].std())
+    plotGraph(''.join(prefix_string.split('/')[-1])+ ' ' + suffix_string.removesuffix('.csv'))
