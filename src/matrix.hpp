82d63c20d6fd557760e2975cd36d09f8ce125353
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-04-17 17:53:11 -0300

    Change `Matrix` data structure for locality

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -1,21 +1,32 @@
 #ifndef _MATRIX_HPP
 #define _MATRIX_HPP
 
+#include <cassert>
 #include <cstddef>
+
+static_assert(sizeof(ushort) == 2, "unsigned short must be 2 bytes");
+static_assert(sizeof(uint) == 4, "unsigned int must be 4 bytes");
 
 template <typename T> class Matrix {
 public:
-  size_t width;
-  size_t height;
+  ushort width;
+  ushort height;
 
-  Matrix(size_t width, size_t height) : width(width), height(height), elems(width * height){};
+  Matrix(ushort width, ushort height) : width(width), height(height) {
+    uint sizei_width = 32 - __builtin_clz(width);
+    uint sizei_height = 32 - __builtin_clz(height);
+    uint sizei = std::max(sizei_width, sizei_height);
+    uint size = (1 << sizei)*(1 << sizei);
 
-  T operator[](size_t index1, size_t index2) const {
-    return elems[index2 * width + index1];
+    elems = std::vector<T>(size);
   };
 
-  T& operator[](size_t index1, size_t index2) {
-    return elems[index2 * width + index1];
+  T operator[](ushort index1, ushort index2) const {
+    return elems[position(index1, index2)];
+  };
+
+  T& operator[](ushort index1, ushort index2) {
+    return elems[position(index1, index2)];
   };
 
   bool operator==(const Matrix& other) const {
@@ -23,9 +34,9 @@ public:
       return false;
     }
 
-    for (size_t i = 0; i < width; i++) {
-      for (size_t j = 0; j < height; j++) {
-        if (elems[j * width + i] != other.elems[j * width + i]) {
+    for (ushort i = 0; i < width; i++) {
+      for (ushort j = 0; j < height; j++) {
+        if (elems[position(i, j)] != other.elems[position(i, j)]) {
           return false;
         }
       }
@@ -35,6 +46,22 @@ public:
   };
 
   std::vector<T> elems;
+
+private:
+  uint position(ushort index1, ushort index2) const {
+    // interleave the bits of index1 and index2
+
+    uint index = 0;
+
+    for (uint i = 0; i < 16; i++) {
+      index |= ((index1 >> i) & 1) << (2 * i);
+      index |= ((index2 >> i) & 1) << (2 * i + 1);
+    }
+
+    assert(index < elems.size());
+
+    return index;
+  };
 };
 
 #endif
