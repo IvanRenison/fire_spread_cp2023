3bf5913bb8602416597684d374157427af3fcdd4
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-04-23 18:12:14 -0300

    Add file with `.cpp` includes for optimal compilation

---
+++ src/predParams/predParams_main_singleFile.cpp
@@ -0,0 +1,8 @@
+#include "csv.cpp"
+#include "fires.cpp"
+#include "ignition_cells.cpp"
+#include "landscape.cpp"
+#include "many_simulations.cpp"
+#include "spread_functions.cpp"
+#include "predParams.cpp"
+#include "predParams_main.cpp"
